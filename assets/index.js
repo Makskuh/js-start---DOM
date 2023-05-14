c4537867629a770b6a7fffc8fb8c7caf59223f79
Max Kuh <maxes2805582@gmail.com>
2023-05-14 18:23:21 +0300

    Add costyl solution

--- assets/index.js
+++ assets/index.js
@@ -1,27 +1,32 @@
 'use strict';
 const img = document.querySelector('#img');
-const btn = document.querySelector('.switch');
-const imgArr = [
-  { src: './assets/img/vaporwave-retro-art.jpg', alt: 'retro' },
-  {
-    src: './assets/img/fon_pikseli_raznotsvetnyj_47113_800x600.jpg',
-    alt: 'piksel',
-  },
-];
-const crtAtr = document.createAttribute('alt');
-crtAtr.value = 'new btn';
-btn.setAttributeNode(crtAtr);
-let currentAnimal = 0;
-const newElem = document.createElement('p');
-newElem.textContent = 'this new element from JS';
-document.body.append(newElem);
-document.body.children
-function btnHandler(event) {
-  currentAnimal = currentAnimal === 0 ? 1 : 0;
-  btn.classList.toggle('togle');
-  img.setAttribute('alt', imgArr[currentAnimal].alt);
-  img.setAttribute('src', imgArr[currentAnimal].src);
-
-  img.hidden = currentAnimal === 0 ?1 :0;
-}
-btn.addEventListener('click', btnHandler);
+const yourTask = document.querySelector('.question-btn');
+const todoList = document.querySelector('.article-todo');
+const deleteBtn = document.querySelector('.btn');
+let newTask;
+let newSection ;
+let newButtonDelete;
+yourTask.addEventListener('click', function () {
+let newElem;
+newTask = prompt('Write new task');
+if(newTask != '') {
+  newSection = document.createElement('section');
+  newSection.classList.add('section-question');
+  newButtonDelete = document.createElement('button');
+  newButtonDelete.classList.add('btn');
+  newButtonDelete.innerText = 'Delete'
+  todoList.append(newSection)
+  newElem = document.createElement('p')
+  newElem.Textcontent = (newTask);
+  newElem.classList.add('text');
+  newSection.append(newTask);
+  newSection.append(newButtonDelete);
+} else
+alert('Write something')
+});
+todoList.addEventListener('click', function(event) {
+  if(event.target !== yourTask) {
+  let elem = event.target;
+  elem.parentElement.remove();
+  } else undefined;
+})
