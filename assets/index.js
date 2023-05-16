947bb2e2a39e17db1b5aa09ccf04f2f2a0c189ae
Max Kuh <maxes2805582@gmail.com>
2023-05-16 18:29:30 +0300

    Add * solution

--- assets/index.js
+++ assets/index.js
@@ -1,28 +1,35 @@
 'use strict';
-const yourTask = document.querySelector('.question-btn');
 const todoList = document.querySelector('.article-todo');
-function deleteHandler(event){
-  event.target.parentElement.remove()
+const form = document.querySelector('#form');
+const btnSubmit = document.querySelector('#submit');
+
+function deleteHandler(event) {
+  event.target.parentElement.remove();
+}
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const { target: formElements } = e;
+  const { elements } = formElements;
+  if(elements.newTask.value === ''||elements.newTask.value === ' '){
+    throw new TypeError('Write something');
+    return
   }
-
-yourTask.addEventListener('click', function () {
-
-const newTask = prompt('Write new task');
-if(newTask != '') {
   const newSection = document.createElement('section');
   newSection.classList.add('section-question');
-  
+
   const newButtonDelete = document.createElement('button');
   newButtonDelete.classList.add('btn');
   newButtonDelete.innerText = 'Delete';
-  newButtonDelete.addEventListener('click',deleteHandler)
-  todoList.append(newSection)
-  const newElem = document.createElement('p')
-  newElem.Textcontent = (newTask);
-  newElem.classList.add('text');
-  newSection.append(newTask);
-  newSection.append(newButtonDelete);
-} else
-alert('Write something')
-});
+  newButtonDelete.addEventListener('click', deleteHandler);
 
+  todoList.append(newSection);
+
+  const newElem = document.createElement('p');
+  newElem.textContent = elements.newTask.value;
+  newElem.classList.add('text');
+
+  newSection.append(newElem);
+  newSection.append(newButtonDelete);
+  elements.newTask.value = '';
+});
